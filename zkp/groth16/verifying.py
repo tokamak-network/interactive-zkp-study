d3f4ccc59a47873a3ce34bc85b97e8f79ec063e0
ggs134 <inditow@gmail.com>
2024-08-31 03:58:36 +0800

    groth16 verifying added

---
+++ zkp/groth16/verifying.py
@@ -0,0 +1,32 @@
+from py_ecc.fields import bn128_FQ as FQ
+from py_ecc import bn128
+
+from random import randint
+
+g1 = bn128.G1
+g2 = bn128.G2
+
+# FR = FQ
+# FR.field_modulus = bn128.curve_order
+
+class FR(FQ):
+    field_modulus = bn128.curve_order
+
+# Elliptic Curve operations
+mult = bn128.multiply
+pairing = bn128.pairing
+add = bn128.add
+neg = bn128.neg
+
+def verify(numWires, proof_A, proof_B, proof_C, sigma1_1, sigma1_3, sigma2_1, Rx):
+    LHS = pairing(proof_B, proof_A)
+    RHS = pairing(sigma2_1[0], sigma1_1[0])
+
+    temp = None
+
+    for i in [0, numWires-1]:
+        temp = add(temp, mult(sigma1_3[i], int(Rx[i])))
+
+    RHS = (RHS * pairing(sigma2_1[1], temp)) * pairing(sigma2_1[2], proof_C)
+
+    return LHS == RHS
